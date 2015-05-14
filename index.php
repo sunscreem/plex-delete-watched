5ad903f26c6a74b3bdbc301ee90fa78424ca57fb
Robert Cooper <rob.cooper@sellonlinedirect.com>
2015-05-14 12:19:26 +0100

    Added compasses sass-bootstrap

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <html>
 <head>
-<title>Plex Server - Watched Programs</title>
+	<title>Plex Server - Watched Programs</title>
+	<link href="css/styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <h1>Hello World.</h1>
+
+
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+ <script src="js/bootstrap.js"></script>
 </html>
