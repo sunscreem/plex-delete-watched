e66411fd1bc93f3e42908d553b28efd9dc9dab29
Robert Cooper <rob.cooper@sellonlinedirect.com>
2015-05-17 20:10:39 +0100

    Initial Testing

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Very basic code to generate a list of watched TV shows that can be deleted on a 
 My hard drive got full and I couldn't find a simple way to delete shows that have been watched without manually working through folders and comparing with watched/unwatched state in plex.
 This has become tedious since the addition of plex home where more and one person may or may not have watched the same show.
 
-There are other projects that are more comprehensive and this but they required a knowledge of python and dont seem to handle a Plex Home setup, where two people watch the same shows at different times.
+There are other projects that are more comprehensive than this but they required a knowledge of python and dont seem to handle a Plex Home setup, where several people can watch the same shows at different times.
 
 <h1>Progress</h1>
 At this stage I'm just trying to generate a basic list of watched shows that are 'safe to delete'.
--- index.php
+++ index.php
@@ -4,10 +4,102 @@
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<h1>Hello World.</h1>
-
+<div class="container">
+	<h1>Hello World - Initial Testing.</h1>
+	
+	<input type="button" onClick="getAuthCode();" value="Get Auth Code"><br><br>
+	<input type="button" onClick="doAThing();" value="Do a thing"><br><br>
+	<input type="button" onClick="doSomething();" value="Start"> 
+	<input type="hidden" value="" id="authcode"><br>
+	<!-- <input type="button" onClick="doNextThing()" value="Do The Next Thing"> -->
+	<h2>Output Here:</h2>
+	<pre id="output"></pre>
+	(Some output goes to the console)
+</div>
 
 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
  <script src="js/bootstrap.js"></script>
+ <script src="config.js"></script>
+ <script>
+
+
+	function getAuthCode(){
+
+			
+			$.ajax({
+				headers:{
+					'Authorization' : 'Basic '+userpass, 
+					'X-Plex-Client-Identifier' : 'RobsPlexWebClient-1',
+					'X-Plex-Product' : 'RobsPlexWebClient',
+					'X-Plex-Version' : '1'
+				},
+				
+				type: 'post',
+				url: 'https://plex.tv/users/sign_in.json',
+				success: function(result){
+					auth_token = result.user.authentication_token;
+					$('#output').append('Auth Token: '+auth_token+'\n\n');
+					$('#authcode').val(auth_token);
+
+				},
+				error : function(xhr,textStatus,errorThrown){
+					$('#output').append('ERROR: '+xhr.status+' '+textStatus+' '+errorThrown+'\n');
+				}
+			});
+	}
+
+	function doAThing(){
+
+
+			$.ajax({
+				headers:{
+					'Authorization' : 'Basic '+userpass, 
+					'X-Plex-Client-Identifier' : 'RobsPlexWebClient-1',
+					'X-Plex-Product' : 'RobsPlexWebClient',
+					'X-Plex-Version' : '1'
+				},
+				
+				type: 'get',
+				url: 'https://plex.tv/api/users',
+				success: function(result){
+					console.log(result);
+					
+
+				}
+			});
+	}
+	
+
+ 	function doSomething(){
+
+ 			$.ajax({
+				headers:{
+					'Authorization' : 'Basic '+userpass,
+					'X-Plex-Client-Identifier' : 'RobsPlexWebClient-1',
+					'X-Plex-Product' : 'RobsPlexWebClient',
+					'X-Plex-Version' : '1',
+					'X-Plex-Device-Name' : 'RobsPlexWebClient-1',
+					'X-Plex-Token': $('#authcode').val(),
+					'Accept' : 'application/json'
+				},
+ 				type: 'get',
+				url: 'http://192.168.0.14:32400/library/sections',
+				success: function(result){
+					$.each(result._children,function(count,child){
+						$('#output').append(child.title+"\n");
+					});
+					
+					
+					
+				},
+				error : function(xhr,textStatus,errorThrown){
+					$('#output').append('ERROR: '+xhr.status+' '+textStatus+' '+errorThrown+'\n');
+				}
+				
+ 			});
+ 	}
+
+
+</script>
 </html>
